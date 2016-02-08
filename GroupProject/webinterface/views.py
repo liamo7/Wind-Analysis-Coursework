f638ba12a8950583ae33d144741ee07f3f371c02
Liam O'Hanlon <liamoh@outlook.com>
2016-02-08 20:00:04 +0000

    gitignore should now work.

--- GroupProject/webinterface/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def main(request):
-    return render(request, 'webinterface/main.html')
-
-def index(request):
-    return HttpResponse("Index")
